f35e2eff5fb261aa025501f1470a36ecaa53d607
Beverly Hernandez <beverlyandez@gmail.com>
2020-06-05 14:06:59 -0400

    add inventory command path to commands index

--- commands/index.js
+++ commands/index.js
@@ -5,7 +5,8 @@ module.exports = {
   Ability: require('./ability.js'),
   Stu: require('./stu.js'),
   Help: require('./help.js'),
-  // Class: require('./class.js'),
+  Class: require('./class.js'),
   Points: require('./points.js'),
-  Coin: require('./coinflip.js')
+  Coin: require('./coinflip.js'),
+  Inventory: require('./inventory/')
 };
