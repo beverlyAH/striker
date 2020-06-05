1f45bad70590d5dbd3dcb4e855c558db00b74681
Beverly Hernandez <beverlyandez@gmail.com>
2020-06-05 14:01:17 -0400

    change roll determinations

--- commands/roll.js
+++ commands/roll.js
@@ -1,51 +1,48 @@
 const rollD6 = require('../utils/roller.js')
-
+const mirssaFinish = (msg, roll) => {
+  let message = `<@${msg.author.id}>: ${roll}`;
+  let outcome = roll.split('`')[1]
+  switch (outcome) {
+    case 'BAD OUTCOME':
+      message += `. I'm so very sorry, sir!`;
+      break;
+    case 'failure...':
+      message += ` My condolences, sir.`;
+      break;
+    case 'partial success':
+      message += ` I'm sure you'll do better next time, sir.`;
+      break;
+    case 'SUCCESS!':
+      message += ` Magnificent, master!`
+      break;
+    case 'CRITICAL SUCCESS!!!':
+      message += ` I've never seen such an amazing display in all my \`ERROR YEARS_OF_SERVICE UNDEFINED\``;
+      break;
+    default:
+      break;
+  }
+  return message
+}
 module.exports = {
   name: '/s',
   description: 'dice roller',
   execute(msg, args) {
-    console.log(args)
-    if(args.length > 0 && args[0] !== '1') {
-      var number = parseInt(args[0])
-      var roll = rollD6(number)
-      if(roll === 'invalid') {
-        msg.channel.send('Please enter a smaller number.')
-        return
-      }
-      var max = parseInt(roll.slice(roll.length - 3))
-      if(max === 6) {
-        if(msg.author.id === '656284156560408580') {
-          msg.channel.send(`<@${msg.author.id}>: \`${number}D6\` => ${roll}  \`Congratulations on a magnificent success, sir.\``)
-        } else {
-          msg.channel.send(`<@${msg.author.id}>: \`${number}D6\` => ${roll}  \`SUCCESS!\``)
-        }
-      } else if (max === 5) {
-        msg.channel.send(`<@${msg.author.id}>: \`${number}D6\` => ${roll}  \`partial success\``)
-      } else {
-        if(msg.author.id === '656284156560408580') {
-          msg.channel.send(`<@${msg.author.id}>: \`${number}D6\` => ${roll}  \`My condolences, sir.\``)
-        } else {
-          msg.channel.send(`<@${msg.author.id}>: \`${number}D6\` => ${roll}  \`failure...\``)
-        }
-      }
+    let roll = rollD6(parseInt(args[0]))
+    if(roll === 'invalid') {
+      msg.channel.send(`Please choose a smaller number of dice to roll.`)
+      .catch(err => { console.error(err) })
+      return 200
+    }
+    //  Mirssa === '656284156560408580'
+    if(msg.author.id === '656284156560408580') {
+      let message = mirssaFinish(msg, roll)
+      msg.channel.send(message)
+      .catch(err => { console.error(err) })
+      return 200
     } else {
-      var rolled = rollD6()
-      var roll = parseInt(rolled.slice(2, rolled.length - 2))
-      if(roll === 6) {
-        if(msg.author.id === '656284156560408580') {
-          msg.channel.send(`<@${msg.author.id}>: \`1D6\` => ${rolled}  \`Congratulations on a magnificent success, sir.\``)
-        } else {
-          msg.channel.send(`<@${msg.author.id}>: \`1D6\` => ${rolled}  \`SUCCESS!\``)
-        }
-      } else if (roll === 5) {
-        msg.channel.send(`<@${msg.author.id}>: \`1D6\` => **${rolled}** \`partial success\``)
-      } else {
-        if(msg.author.id === '656284156560408580') {
-          msg.channel.send(`<@${msg.author.id}>: \`1D6\` => ${rolled}  \`My condolences, sir.\``)
-        } else {
-          msg.channel.send(`<@${msg.author.id}>: \`1D6\` => ${rolled}  \`failure...\``)
-        }
-      }
+      msg.channel.send(`<@${msg.author.id}>: ${roll}`)
+      .catch(err => { console.error(err) })
+      return 200
     }
   }
 };
--- utils/roller.js
+++ utils/roller.js
@@ -1,21 +1,82 @@
+const isCriticalSuccess = (array) => {
+  let count = 0
+  for (let i = 0; i < array.length; i++) {
+    if(array[i] === 6) {
+      count++
+    }
+  }
+  return count > 1
+}
+
+const appendMessage = (string, arr, disadvantage) => {
+  let selection;
+  if(disadvantage) {
+    selection = Math.min(...arr)
+  } else {
+    selection = Math.max(...arr)
+  }
+
+  if (isCriticalSuccess(arr)) {
+    string += ' `CRITICAL SUCCESS!!!`';
+    return string;
+  } else {
+    switch (selection) {
+      case 1:
+        string += ' `BAD OUTCOME`';
+        return string;
+      case 2:
+      case 3:
+        string += ' `failure...`';
+        return string;
+      case 4:
+      case 5:
+        string += ' `partial success`';
+        return string;
+      case 6:
+        string += ' `SUCCESS!`'
+        return string;
+      default:
+        return string;
+    }
+  }
+}
+
 module.exports = (n) => {
-  if (n > 456) {
+  let final;
+  // error protection 
+  if (n > 350) {
     return 'invalid'
   }
-  if(n === undefined || typeof n !== 'number' || Number.isNaN(parseInt(n)) || n === '1') {
-    return '**' + Math.floor(Math.random() * 6 + 1) + '**'
-  } else {
-    var numbers = ''
-    var arr = []
-    for (var i = 0; i < n; i++) {
-      var number = Math.floor(Math.random() * 6 + 1)
-      arr.push(number)
-      if(number === 6 || number === 5) {
-        number = '**' + number + '**'
-      }
-      numbers += number.toString() + ', '
-    }
-    return numbers.slice(0, numbers.length - 2).toString() +
-    ' => **' + Math.max(...arr).toString() + '**'
+
+  // roll for disadvantage
+  if (n === 0) {
+    let rolls = []
+    rolls.push(Math.floor(Math.random() * 6 + 1))
+    rolls.push(Math.floor(Math.random() * 6 + 1))
+    let result = Math.min(...rolls)
+    final = `\`0D6\` => ${rolls[0]}, ${rolls[1]} => **${result}**`
+    return appendMessage(final, rolls, true);
   }
+
+  // no number, or something that's not a number
+  if (n === undefined || typeof n !== 'number' || Number.isNaN(parseInt(n)) || parseInt(n) === 1) {
+    let singleRoll = Math.floor(Math.random() * 6 + 1)
+    final = '`1D6` => **' + singleRoll + '**';
+    return appendMessage(final, [singleRoll], false);
+  }
+
+  // default case
+  let numbers = ''
+  let arr = []
+  for (let i = 0; i < n; i++) {
+    let number = Math.floor(Math.random() * 6 + 1)
+    arr.push(number)
+    if (number === 6) {
+      number = '**' + number + '**'
+    }
+    numbers += number.toString() + ', '
+  }
+  final = numbers.slice(0, numbers.length - 2).toString() +
+  ' => **' + Math.max(...arr).toString() + '**'
+  return appendMessage(final, arr, false)
 }
